c88af227aa99cd4f0f12e1b296094597c2da9f22
Millena Sui <millena.sui@outlook.com>
2023-10-09 12:33:38 -0300

    correctMain

--- busca.c
+++ busca.c
@@ -2,28 +2,39 @@
 #include <stdlib.h>
 #include "avl-tree.h"
 
-int main()
-{
-    // Cria uma árvore AVL vazia
+int main() {
+    
+    //Cria uma árvore AVL vazia
     arvore *avl = inicializaArvore();
-    char comando; // i, r ou b
-    int chave;    // Chave do nó
 
-    // Lê os comandos e chaves
-    while (scanf(" %c %d", &comando, &chave) == 2)
-    {
-        // insere nó na árvore
+    char comando;
+    int chave;
+
+    while (scanf(" %c %d", &comando, &chave) == 2) {
+
         if (comando == 'i') {
+            //Insere nó na árvore
             nodo *novo = inicializaNodo(chave);
             avl = insereNodo(avl, novo);
+            imprimeArvore(avl->raiz);
+            printf("\n");
 
-            // remove nó da árvore
         } else if (comando == 'r') {
+            //Remove nó da árvore
             avl = removeNodo(avl, chave);
+            imprimeArvore(avl->raiz);
+            printf("\n");
 
-            // busca nó na árvore
         } else if (comando == 'b') {
+            
+            //Se árvore é vazia, indica erro
             if (avl->raiz != NULL) {
+            
+                //Se nó não está na árvore, indica erro
+                if (buscaNodo(avl->raiz, chave) == 0)
+                    fprintf(stderr, "Nó não encontrado.\n");
+
+                //Imprime os nós consultados na busca
                 nodo *nodoEncontrado = avl->raiz;
                 printf("%d", nodoEncontrado->chave);
 
@@ -38,12 +49,13 @@ int main()
                         printf(",%d", nodoEncontrado->chave);
                 }
                 printf("\n");
-            }
+
+            } else
+                fprintf(stderr, "Nó não encontrado: Árvore vazia.\n");
         }
-        imprimeArvore(avl->raiz);
-        printf("\n");
     }
-    // Libera a memória da árvore
+
+    //Libera a memória da árvore
     destroiArvore(avl);
     return 0;
 }
